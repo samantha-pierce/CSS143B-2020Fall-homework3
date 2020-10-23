19d7833e85f8386fbb1545a3c9498d704a4bccc5
Samantha-Pierce <spapierce@gmail.com>
2020-10-23 14:36:13 -0700

    implemented ValidParentheses and passes tests

--- src/main/java/Problem1/ValidParentheses.java
+++ src/main/java/Problem1/ValidParentheses.java
@@ -6,6 +6,32 @@ public class ValidParentheses {
     public static boolean isValid(String str) {
         // homework
         // !!! must use ArrayStack or LinkedListStack from problem 1
-        return false; // place holder
+        if (str == null || str.length() == 0) {
+            return true;
+        }
+        LinkedListStack<Character> stack = new LinkedListStack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else if (c == ')' || c == ']' || c == '}') {
+                // by checking the size, this makes sure the first character of the string pushed
+                // on the stack was an open bracket or parenthesis
+                if (stack.size() > 0) {
+                    if (stack.peek().equals('(') && c == ')') {
+                        stack.pop();
+                    } else if (stack.peek().equals('[') && c == ']') {
+                        stack.pop();
+                    } else if (stack.peek().equals('{') && c == '}') {
+                        stack.pop();
+                    }
+                }
+            } else {
+                // false if the string did not contain only parenthesis or brackets
+                return false;
+            }
+        }
+        // if stack is empty, then all parenthesis matched with each other and returns true
+        return stack.size() == 0;
     }
 }
