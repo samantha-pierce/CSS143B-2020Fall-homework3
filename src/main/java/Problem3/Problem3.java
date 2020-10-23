1f4e42fc1fbff9d86b064d3c2314f8856beac3a5
Samantha-Pierce <spapierce@gmail.com>
2020-10-23 09:57:37 -0700

    implemented Problem3 method and passes tests

--- src/main/java/Problem3/Problem3.java
+++ src/main/java/Problem3/Problem3.java
@@ -10,7 +10,15 @@ public class Problem3 {
     public static void printListInReverse(ListNode list, PrintStream print) {
         // homework
         // !!! must use your ArrayStack or LinkedListStack in problem 1
-        // print with the following format. System.out.println() will not pass test.
-        print.println("hello world");   // place holder
+        LinkedListStack stack = new LinkedListStack();
+        // prevents the dummy node being pushed onto the stack
+        list = list.next;
+        while (list != null) {
+            stack.push(list.val);
+            list = list.next;
+        }
+        while (stack.size() > 0) {
+            print.println(stack.pop());
+        }
     }
 }
